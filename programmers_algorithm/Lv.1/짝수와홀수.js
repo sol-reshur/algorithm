82259c0d7408e9ab076e1ae2f9a707f84d44b785
sol-reshur <sol.reshur@gmail.com>
2022-03-18 15:42:01 +0900

    first_algorithm

---
+++ programmers_algorithm/Lv.1/짝수와홀수.js
@@ -0,0 +1,17 @@
+/*
+문제설명 :
+정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+제한조건 :
+num은 int 범위의 정수입니다.
+num은 int 범위의 정수입니다. */
+
+function solution(num){
+    if( num % 2 === 0) {
+
+        return "Even";
+            } else{
+
+                return "Odd";
+            }
+}
